e3515449e4657eb7e133edd0d0872c2731cd1267
lishaoping <1913796030@qq.com>
2017-03-20 20:35:10 +0800

    add mapping to project

---
+++ src/main/java/com/yunhai/dao/UserDao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.yunhai.dao;
+
+import java.util.List;
+
+import com.yunhai.model.UserEntity;
+
+/**
+ * @author Administrator
+ * 2017年3月19日
+ * UserDao
+ */
+
+public interface UserDao {
+
+	List<UserEntity> getAllUser();
+}
