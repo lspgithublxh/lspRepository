df4775cc326792baf17e19da6cdbab454489524d
lishaoping <1913796030@qq.com>
2017-04-08 11:39:04 +0800

    spring-boot project start

---
+++ src/main/java/com/yunhai/boot/App.java
@@ -0,0 +1,28 @@
+package com.yunhai.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author lishaoping
+ * 2017年4月8日上午11:26:34
+ * App
+ */
+@SpringBootApplication
+@RestController//(value="boot")
+public class App 
+{
+	@RequestMapping(value="/")
+	public String hello(){
+		return "Hello World!";
+	}
+	
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
