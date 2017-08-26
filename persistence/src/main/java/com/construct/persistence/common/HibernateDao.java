bc7db4fb6e3434fcbd7b01f496a862082d87de43
lsp <1913796030@qq.com>
2017-08-26 11:27:36 +0800

    增加freemarker到hibernate

--- persistence/src/main/java/com/construct/persistence/common/HibernateDao.java
+++ persistence/src/main/java/com/construct/persistence/common/HibernateDao.java
@@ -1,7 +1,78 @@
 package com.construct.persistence.common;
 
+import java.util.List;
+import java.util.Map;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("hibernateDao")
 public class HibernateDao {
 
-//	private SessionFactory sessionFactory; 
+	@Autowired
+	private SessionFactory sessionFactory; 
 	
+	private Session getCurrentSession() {
+        return this.sessionFactory.openSession();
+    }
+	
+	@SuppressWarnings("unchecked")
+	public <T> List<T> queryForListByNamedHql(String hql, Map<String, String> param, Class<T> cla_){
+		Query<T> query = getCurrentSession().getNamedQuery(hql);
+		for(String key : param.keySet()){
+			query.setParameter(key, param.get(key));
+		}
+		List<T> result = query.list();
+		return result;
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<?> queryForListByNamedHql2(String hql, Map<String, String> param, Class<? extends Object> cla_){
+		Query<?> query = getCurrentSession().getNamedQuery(hql);
+		for(String key : param.keySet()){
+			query.setParameter(key, param.get(key));
+		}
+		List<?> result = query.list();
+		return result;
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public <K> List<K>  queryForListByHql(String hql, Class<K> cla_) {
+		Query<K> query = getCurrentSession().createQuery(hql, cla_);
+		return query.list();
+	}
+	
+	public List<?> queryForListByNamedSql(String sql, Map<String, String> params, Class<? extends Object> cla_){
+		Query<?> query = getCurrentSession().getNamedQuery(sql);
+		for(String key : params.keySet()){
+			query.setParameter(key, params.get(key));
+		}
+		List<?> result = query.list();
+		return result;
+	}
+	
+	public <T> void test(T... args) {
+		for(T t: args) {
+			System.out.println(t.getClass());
+		}
+	}
+	
+	interface CC<L>{
+		public L getString();
+	}
+	
+	class DD<T> implements CC<T>{
+
+		private T x;
+		@Override
+		public T getString() {
+			return null;
+		}
+		
+	}
 }
---
+++ persistence/src/main/java/com/construct/persistence/entity/UserRoles.java
@@ -0,0 +1,35 @@
+package com.construct.persistence.entity;
+
+/**
+ * 
+ *
+ * @author lishaoping
+ *persistence
+ *2017年8月26日
+ */
+public class UserRoles {
+
+	private String id;
+	private String username;
+	private String role;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	
+}
