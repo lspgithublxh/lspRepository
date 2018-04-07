37a9b94b0ba96f8b6a9b41537b1c2fcfffcceab2
lishaoping <1913796030@qq.com>
2018-04-07 16:18:27 +0800

    彻底解决pycharms包找不到问题

--- base/spark_handle/text_source.py
+++ base/spark_handle/text_source.py
@@ -8,6 +8,7 @@ sc = SparkContext("local", "Page Rank")
 
 #文件中读取,构成RDD  PythonRDD
 distFile = sc.textFile("README.txt")
+distFile.persist().cache()
 #文本长度
 print distFile.map(lambda line : len(line)).reduce(lambda a, b : a + b)
 #单词个数
@@ -15,5 +16,6 @@ print distFile.map(lambda line : len(re.split("\s+", line))).reduce(lambda a, b 
 #分割统计, reduceBykey、sortByKey要求在(key,val)对上进行
 print distFile.flatMap(lambda line : re.split("\s+", line)).map(lambda word : (word, 1)) \
         .reduceByKey(lambda count1 , count2: count1 + count2).map(lambda tu : (tu[1], tu[0])).sortByKey(ascending=False).collect()
+
 
 
