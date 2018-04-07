24c28a72b18c1be9d9f4ef0584cb8e9f9b318760
lishaoping <1913796030@qq.com>
2018-04-07 15:19:31 +0800

    add spark handle

--- base/spark_handle/base_handle.py
+++ base/spark_handle/base_handle.py
@@ -1,1 +1,12 @@
 #coding=utf-8
+from pyspark import SparkContext, SparkConf
+import os
+
+os.environ['HADOOP_HOME'] = "D:\download\spark-2.3.0-bin-hadoop2.7\spark-2.3.0-bin-hadoop2.7\HADOOP_HOME"
+
+sc = SparkContext("local", "Page Rank")
+data = [1,2,3,4,5]
+#创建分布式数据集RDD
+distData = sc.parallelize(data)
+#求和
+print distData.reduce(lambda  a, b: a + b)
---
+++ base/spark_handle/text_source.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+from pyspark import SparkContext, SparkConf
+import os
+import re
+
+os.environ['HADOOP_HOME'] = "D:\download\spark-2.3.0-bin-hadoop2.7\spark-2.3.0-bin-hadoop2.7\HADOOP_HOME"
+sc = SparkContext("local", "Page Rank")
+
+#文件中读取,构成RDD  PythonRDD
+distFile = sc.textFile("README.txt")
+#文本长度
+print distFile.map(lambda line : len(line)).reduce(lambda a, b : a + b)
+#单词个数
+print distFile.map(lambda line : len(re.split("\s+", line))).reduce(lambda a, b : a + b)
+#分割统计, reduceBykey、sortByKey要求在(key,val)对上进行
+print distFile.flatMap(lambda line : re.split("\s+", line)).map(lambda word : (word, 1)) \
+        .reduceByKey(lambda count1 , count2: count1 + count2).map(lambda tu : (tu[1], tu[0])).sortByKey(ascending=False).collect()
+
+
