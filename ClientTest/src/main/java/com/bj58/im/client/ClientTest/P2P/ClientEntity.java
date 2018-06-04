7c7afd9825e7a39bb7d567fcc089c69afbecac8c
lishaoping <1913796030@qq.com>
2018-06-04 10:14:49 +0800

    p2p尝试2

--- ClientTest/src/main/java/com/bj58/im/client/ClientTest/P2P/ClientEntity.java
+++ ClientTest/src/main/java/com/bj58/im/client/ClientTest/P2P/ClientEntity.java
@@ -5,7 +5,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
@@ -15,7 +18,7 @@ public class ClientEntity {
 
 	public static void main(String[] args) {
 		try {
-			new ClientEntity(19999).test();
+			new ClientEntity(19999, 0).test();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -23,17 +26,26 @@ public class ClientEntity {
 	
 	int serverPort;
 
-	public ClientEntity(int serverPort) {
+	
+	public ClientEntity(int serverPort, int localport) {
 		super();
 		this.serverPort = serverPort;
+		this.localport = localport;
 	}
 
-
+	int localport = 0;//会被反复使用
 
 	private void test() throws UnknownHostException, IOException {
-		Socket socket = new Socket("localhost", serverPort);
+		Socket socket = new Socket();
+		if(0 != localport) {
+			System.out.println("bind localhost port : " + localport);
+			socket.bind(new InetSocketAddress(InetAddress.getLocalHost(), localport));//使用本地的socket端口？
+		}
+		socket.setReuseAddress(true);//先设置，后连接
+		socket.connect(new InetSocketAddress(InetAddress.getLocalHost(), serverPort));
 		System.out.println("start connect to localhost, 19999 ;current port:" + socket.getLocalPort());
-		socket.setReuseAddress(true);
+
+		localport = socket.getLocalPort();
 		OutputStream out = socket.getOutputStream();
 		InputStream in = socket.getInputStream();
 		
@@ -98,9 +110,9 @@ public class ReadThread extends Thread{
 					while(true) {
 						String line = dataIn.readUTF();
 						System.out.println("server:" + line);
-						if(line.startsWith("127.0.0.1:")) {
+						if(line.startsWith("127.0.0.1:") || line.startsWith("10.252.62.125:")) {
 							System.out.println("start client to client: " + line);
-							new ClientEntity(Integer.valueOf(line.substring(line.indexOf(":") + 1))).test();
+							new ClientEntity(Integer.valueOf(line.substring(line.indexOf(":") + 1)), localport).test();
 						}
 //						
 					}
