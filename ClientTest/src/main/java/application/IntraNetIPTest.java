2f96ad13f1016e711d0ea10791d8a7b28b73f24a
lishaoping <1913796030@qq.com>
2018-08-08 10:00:27 +0800

    折线图

--- ClientTest/src/main/java/application/IntraNetIPTest.java
+++ ClientTest/src/main/java/application/IntraNetIPTest.java
@@ -5,9 +5,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 局域网搜索动态ip
+ * 动态网络命令，其他网络命令先省略
  * @ClassName:IntraNetIPTest
  * @Description:
  * @Author lishaoping
@@ -19,12 +22,20 @@ public class IntraNetIPTest {
 
 	public static void main(String[] args) {
 		try {
+			Pattern pattern  = Pattern.compile("\\s+((?:\\d+\\.){3}\\d+)\\s+(\\S+)\\s+(\\S+)\\s+");
 			Process p = Runtime.getRuntime().exec("arp -a");
 			BufferedInputStream in = new BufferedInputStream(p.getInputStream());
 			BufferedReader reader = new BufferedReader(new InputStreamReader(in, Charset.forName("GBK")));
 			String line = "";
 			while((line = reader.readLine()) != null) {
-				System.out.println(new String(line.getBytes(), Charset.forName("UTF-8")));
+				String hang = new String(line.getBytes(), Charset.forName("UTF-8"));
+//				System.out.println(hang);//提取出ip
+				Matcher m = pattern.matcher(hang);
+				if(m.find()) {
+					System.out.println(m.group(1));
+					System.out.println(m.group(2));
+					System.out.println(m.group(3));
+				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
---
+++ ClientTest/src/main/java/com/bj58/im/client/mediaTest/VideoTest.java
@@ -0,0 +1,160 @@
+package com.bj58.im.client.mediaTest;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.TargetDataLine;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
+import javafx.stage.Stage;
+
+/**
+ * lame.exe
+ * 可以成功检测到波形
+ * @ClassName:VideoTest
+ * @Description:
+ * @Author lishaoping
+ * @Date 2018年8月7日
+ * @Version V1.0
+ * @Package com.bj58.im.client.mediaTest
+ */
+public class VideoTest extends Application{
+
+	static byte[] buffer;
+	
+	public static void main(String[] args) {
+//		audioTest();
+		launch(args);
+	}
+
+	private static void justChart() {
+		LineChart<Number, Number> chart = new LineChart<>(new NumberAxis(), new NumberAxis());
+		chart.autosize();
+		Series<Number, Number> se = createSerias();
+		Scene scene = new Scene(chart, 600, 400);
+		chart.getData().add(se);
+		Stage stage = new Stage();
+		stage.setTitle("统计图");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private static Series<Number, Number> createSerias() {
+		Series<Number, Number> se = new XYChart.Series<>();
+		
+		AudioFormat af = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 8000f, 16, 1, 16 / 8 * 1, 8000f, true);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, af);
+		TargetDataLine td;
+		try {
+			td = (TargetDataLine) AudioSystem.getLine(info);
+			td.open(af);
+			td.start();
+			byte[] da = new byte[10240];
+			td.read(da, 0, da.length);
+			int index = 0;
+			for(byte d : da) {
+				se.getData().add(new Data<Number, Number>(++index, d));
+			}
+//			new Thread(new Runnable() {
+//				@Override
+//				public void run() {
+//					
+//					Platform.runLater(new Runnable() {
+//						@Override
+//						public void run() {
+//							int index = 0;
+//							byte[] da = new byte[10240];
+//							while(true) {
+//								td.read(da, 0, da.length);
+//								for(byte d : da) {
+//									se.getData().add(new Data<Number, Number>(++index, d));
+//								}
+//								try {
+//									Thread.sleep(1000);
+//									System.out.println("sleep");
+//								} catch (InterruptedException e) {
+//									e.printStackTrace();
+//								}
+//							}
+//						}
+//					});
+//				}}).start();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		
+		
+		se.getData().add(new Data<Number, Number>(1, -1.1));
+		se.setName("统计图");
+		return se;
+	}
+
+	private static void audioTest() {
+		AudioFormat af = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 8000f, 16, 1, 16 / 8 * 1, 8000f, true);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, af);
+		try {
+			//麦克风---是数据源
+			TargetDataLine td = (TargetDataLine) AudioSystem.getLine(info);
+			td.open(af);
+			td.start();
+			
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					byte[] da = new byte[10240];
+					while(true) {
+						td.read(da, 0, da.length);
+						showdata(da);
+						//准备工作
+						AudioInputStream ais = new AudioInputStream(new ByteArrayInputStream(da), af, da.length / af.getFrameSize());
+						DataLine.Info info2 = new DataLine.Info(SourceDataLine.class, af);
+						try {
+							//是音响，是数据目的地
+							SourceDataLine sd = (SourceDataLine) AudioSystem.getLine(info2);
+							sd.open(af);
+							sd.start();
+							buffer = new byte[10240];
+							int len = ais.read(buffer, 0, buffer.length);
+							//写入sd就会播放数据
+							if(len > 0) {
+								sd.write(buffer, 0, len);
+							}
+						} catch (LineUnavailableException e) {
+							e.printStackTrace();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+					}
+				}
+
+				private void showdata(byte[] da) {
+					for(byte d : da) {
+						System.out.print(d + ",");
+					}
+					System.out.println();
+				}
+			}).start();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		justChart();
+	}
+}
