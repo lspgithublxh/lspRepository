35e4ec4cb487e1758a8cadcc897a0417f9328d94
lishaoping <lishaoping@58ganji.com>
2018-09-18 16:35:33 +0800

    特征值 和特征向量 的计算 成功

--- ClientTest/src/main/java/com/bj58/im/client/matrix/QyzSjVgDf.java
+++ ClientTest/src/main/java/com/bj58/im/client/matrix/QyzSjVgDf.java
@@ -1,5 +1,11 @@
 package com.bj58.im.client.matrix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * 行列式的计算：具体矩阵 用初等变换；；含有lamda用  从内到外增长的方式进行 逼近 计算
  * @ClassName:QyzSjVgDf
@@ -24,6 +30,14 @@ public class QyzSjVgDf {
 		System.out.println(hls2);
 		Double hls3 = hanglieshi(s3);
 		System.out.println(hls3);
+		//对角化 专门测试
+//		duijiaohua(s);
+//		printMatrix(s);
+//		duijiaohuaQiang(s);
+		printMatrix(s);
+		
+		//计算特征值 和 特征向量
+		bijinHanglishi(s);
 	}
 
 	private static void printMatrix(Double[][] s) {
@@ -73,6 +87,12 @@ public class QyzSjVgDf {
 		}
 	}
 	
+	private static void jiaohuanijPure(Double[][] matrix, int i, int j) {
+		Double[] temp = matrix[i];
+		matrix[i] = matrix[j];
+		matrix[j] = temp;
+	}
+	
 	private static boolean jiaohuanij(Double[][] matrix, int i, int j) {
 		boolean changed = false;
 		for(int c = i; c < matrix.length; c++) {
@@ -87,18 +107,169 @@ public class QyzSjVgDf {
 		return changed;
 	}
 	
+	
 	private static Double hanglieshi(Double[][] matrix) {
-		for(int i = 0; i < matrix.length; i++) {
-			jiaohuanij(matrix, i, i);
-			for(int j = i + 1; j < matrix.length; j++) {
-				chengkaddto(matrix, i, j, - matrix[j][i] / matrix[i][i] );
-			}
-		}
+//		duijiaohua(matrix);
+		duijiaohuaQiang(matrix);
 		//计算对角线元素的乘积
 		Double hls = 1d;
 		for(int i = 0; i < matrix.length; i++) {
 			hls *= matrix[i][i];
 		}
 		return hls;
+	}
+
+	private static void duijiaohua(Double[][] matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			jiaohuanij(matrix, i, i);//会改变行列式符号
+			if(matrix[i][i] == 0) {//为0的一行
+				continue;
+			}
+			for(int j = i + 1; j < matrix.length; j++) {
+				chengkaddto(matrix, i, j, - matrix[j][i] / matrix[i][i] );
+			}
+		}
+	}
+	
+	private static void duijiaohuaQiang(Double[][] matrix) {
+		for(int i = 0; i < matrix.length; i++) {
+			jiaohuanij(matrix, i, i);
+			if(matrix[i][i] == 0) {//为0的一行
+				//往上走：如果有非0的，那么交换;第一次为0就交换
+				boolean hasLou = false;
+				for(int k = i - 1; k >= 0; k--) {
+					if(matrix[k][i] != 0) {
+						jiaohuanijPure(matrix, i, k);
+						hasLou = true;
+						break;
+					}
+				}
+//				if(!hasLou) continue;//其实没必要再动，因为肯定都是0了
+			}
+			for(int j = i + 1; j < matrix.length; j++) {
+				chengkaddto(matrix, i, j, - matrix[j][i] / matrix[i][i] );
+			}
+			
+			for(int j = i - 1; j >= 0; j--) {
+				chengkaddto(matrix, i, j, - matrix[j][i] / matrix[i][i] );
+			}
+		}
+	}
+	
+	private static Double daicanHanglishi(Double[][] matrix, Double lamda) {
+		for(int i = 0; i < matrix.length; i++) {
+			matrix[i][i] -= lamda; 
+		}
+		return hanglieshi(matrix);
+	}
+	
+	private static Double[][] copy(Double[][] matrix) {
+		Double[][] cc = new Double[matrix.length][matrix[0].length];
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix.length; j++) {
+				cc[i][j] = matrix[i][j];
+			}
+		}
+		return cc;
+	}
+
+	static QyzSjVgDf instance = new QyzSjVgDf();
+	
+	private static Double bijinHanglishi(Double[][] matrix) {
+		List<Entity> rs = new ArrayList<>();
+		Double startLeft = -100d;
+		Double startRight = 0d;
+		Double kuan = 100d;
+		Double currIndex = startLeft;
+		boolean left = true;
+		while(true) {
+			for(Double index = 0d; index <= kuan; index += 0.1) {
+				currIndex += index;
+				Double hls = daicanHanglishi(copy(matrix), index);
+				if(Math.abs(hls) < 0.001) {//认为行列式备选 的特征值
+					rs.add(instance.new Entity(Math.abs(hls),index));
+				}
+			}
+			if(rs.size() >= matrix.length) {
+				break;
+			}
+			if(left) {
+				currIndex = startRight;
+				startRight += kuan;
+			}else {
+				startLeft -= kuan;
+				currIndex = startLeft;
+			}
+			left = !left;
+			if(startRight > 3000 || startLeft < -3000) {
+				break;
+			}
+		}
+		Collections.sort(rs, new Comparator<Entity>() {
+			@Override
+			public int compare(Entity o1, Entity o2) {
+				if(o1.hls > o2.hls) {
+					return 1;
+				}else if(o1.hls < o2.hls) {
+					return -1;
+				}
+				return 0;
+			}
+		});
+		System.out.println(rs);
+		//根据行列式 求出 特征值：组合的方式--正交的方式 
+		//假定  rs中的都是 特征值
+		List<Body> zhengjiaojuzhen = new ArrayList<>();
+		for(Entity tz : rs) {
+			Double lamda = tz.lamda;
+			//
+			Double[][] tis = copy(matrix);
+			for(int i = 0; i < tis.length; i++) {
+				tis[i][i] -= lamda;
+			}
+			//对角化
+			duijiaohuaQiang(tis);
+			//求出每个对角阵对应的n-r个特征向量
+			//直接可以算出来！！
+			
+			for(int in  = 0; in < tis.length; in++ ) {
+				if(Math.abs(tis[in][in]) < 0.001) {
+					double[] tzVector = new double[tis[0].length];
+					tzVector[in] = 1d;
+					zhengjiaojuzhen.add(instance.new Body(tzVector, lamda));
+				}
+			}
+		}
+		System.out.println(zhengjiaojuzhen);
+		return null;
+	}
+	
+	class Entity{
+		private Double hls;
+		private Double lamda;
+		public Entity(Double hls, Double lamda) {
+			super();
+			this.hls = hls;
+			this.lamda = lamda;
+		}
+		@Override
+		public String toString() {
+			return "[hls=" + hls + ", lamda=" + lamda + "]";
+		}
+		
+	}
+	
+	class Body{
+		private double[] tzVetor;
+		private Double tzz;
+		public Body(double[] tzVector, Double tzz) {
+			super();
+			this.tzVetor = tzVector;
+			this.tzz = tzz;
+		}
+		@Override
+		public String toString() {
+			return "[tzVetor=" + Arrays.toString(tzVetor) + ", tzz=" + tzz + "]";
+		}
 	}
 }
