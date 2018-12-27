6d1017148096024b20988e7c03139edc357805be
lishaoping <lishaoping@58ganji.com>
2018-12-27 11:49:23 +0800

    add thrift test, more

--- SRCTest/src/main/java/com/bj58/pubthree/thrift/Client.java
+++ SRCTest/src/main/java/com/bj58/pubthree/thrift/Client.java
@@ -2,7 +2,9 @@ package com.bj58.pubthree.thrift;
 
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFastFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
@@ -11,7 +13,25 @@ public class Client {
 
 	public static void main(String[] args) {
 		for(int i = 0; i < 10; i++) {
-			test();
+//			test();
+			test2();
+		}
+	}
+
+	private static void test2() {
+		TTransport soc = new TFastFramedTransport(new TSocket("localhost", 10991));
+		TProtocol proto = new TCompactProtocol(soc);
+		
+		HelloService.Client client = new HelloService.Client(proto);
+		try {
+			soc.open();
+			String rs = client.sayHello("hello, eclipse");
+			System.out.println("result : " + rs);
+			soc.close();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (TException e) {
+			e.printStackTrace();
 		}
 	}
 
--- SRCTest/src/main/java/com/bj58/pubthree/thrift/ThriftTest.java
+++ SRCTest/src/main/java/com/bj58/pubthree/thrift/ThriftTest.java
@@ -2,10 +2,16 @@ package com.bj58.pubthree.thrift;
 
 import org.apache.thrift.TProcessor;
 import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.TNonblockingServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TServerSocket;
 import org.apache.thrift.transport.TTransportException;
+import org.apache.thrift.transport.TTransportFactory;
 
 /**
  * 参考文档：https://www.cnblogs.com/duanxz/p/5516558.html
@@ -26,17 +32,40 @@ public class ThriftTest {
 		TProcessor pros = new HelloService.Processor<HelloService.Iface>(new HelloServiceImpl());
 		
 		try {
-			TServerSocket sok = new TServerSocket(10991);
-			TServer.Args args = new TServer.Args(sok);
-			
+//			one(pros);
+			//2.
+//			two(pros);
+			//3.
+			TNonblockingServerSocket sok = new TNonblockingServerSocket(10991);
+			TNonblockingServer.Args args = new TNonblockingServer.Args(sok);
 			args.processor(pros);
-			args.protocolFactory(new TBinaryProtocol.Factory());
-			
-			TServer server = new TSimpleServer(args);
+			args.protocolFactory(new TCompactProtocol.Factory());
+			args.transportFactory(new TFramedTransport.Factory());
+			TServer server =  new TNonblockingServer(args);
 			server.serve();
 		} catch (TTransportException e) {
 			e.printStackTrace();
 		}
 		
+	}
+
+	private static void two(TProcessor pros) throws TTransportException {
+		TServerSocket sok = new TServerSocket(10991);
+		TThreadPoolServer.Args args = new TThreadPoolServer.Args(sok);
+		args.processor(pros);
+		args.protocolFactory(new TBinaryProtocol.Factory());
+		TServer server = new TThreadPoolServer(args);
+		server.serve();
+	}
+
+	private static void one(TProcessor pros) throws TTransportException {
+		TServerSocket sok = new TServerSocket(10991);
+		TServer.Args args = new TServer.Args(sok);
+		
+		args.processor(pros);
+		args.protocolFactory(new TBinaryProtocol.Factory());
+		
+		TServer server = new TSimpleServer(args);
+		server.serve();
 	}
 }
