e544edd32875e6c8dfdcd36fb295e6228512c200
lishaoping <1913796030@qq.com>
2019-11-28 15:54:02 +0800

    水坑问题

--- KeyListBase/src/main/java/com/li/shao/ping/KeyListBase/datastructure/test/QuestionJishuiTest.java
+++ KeyListBase/src/main/java/com/li/shao/ping/KeyListBase/datastructure/test/QuestionJishuiTest.java
@@ -8,6 +8,8 @@ public class QuestionJishuiTest {
 
 	public static void main(String[] args) {
 		int total = jishuiTotal(new int[][] {{3,2,3},{4,1,5},{1,2,6}});
+		System.out.println(total);
+		
 	}
 
 	private static int jishuiTotal(int[][] arr) {
@@ -18,6 +20,7 @@ public class QuestionJishuiTest {
 		int quanNum = arr.length / 2 + arr.length % 2;
 		int currQuan = 0;
 		List<int[]> xiuz = Lists.newArrayList();
+		List<int[]> lianxuKen = Lists.newArrayList();
 		while(currQuan < quanNum) {
 			int startX = currQuan;
 			int endX = arr.length - 1 - startX;
@@ -101,9 +104,82 @@ public class QuestionJishuiTest {
 				shendu2 = shendu2 < 0 ? 0 : shendu2;
 				shendu[xP][yP] = shendu[xP][yP] > shendu2 ? shendu2 : shendu[xP][yP];
 			}
-			//开始修正
-			
+			//开始修正.两个方向
+			//先定漏洞点
+			while(true) {
+				int kenMowei = -1;
+				boolean startLian = false;
+				int rightDuan = -1;
+				int leftDuan = xiuz.size() - 1;
+				for(int i = 0; i < xiuz.size();) {
+					int[] node = xiuz.get(i);
+					
+					if(shendu[node[0]][node[1]] > 0) {//是坑
+						lianxuKen.add(node);
+						kenMowei = i;
+						startLian = true;
+					}else if(startLian) {
+						rightDuan = i;
+						break;
+					}
+					if(!startLian) {
+						leftDuan = i;
+					}
+					if(lianxuKen.size() == xiuz.size()) {
+						break;
+					}
+					i = ++i % xiuz.size();
+				}
+				if(kenMowei == -1) {//无坑,不修正
+					break;
+				}//寻找最低边界
+				if(lianxuKen.size() == xiuz.size()) {//全是坑
+					//寻找最低高度
+					int gao = Integer.MAX_VALUE;
+					for(int[] no : xiuz) {
+						int ca = arr[no[0]][no[1]] + shendu[no[0]][no[1]];
+						if(ca < gao) {
+							gao = ca;
+						}
+					}
+					//开始修正
+					for(int[] no : xiuz) {
+						int ca = gao - arr[no[0]][no[1]];
+						shendu[no[0]][no[1]] = ca < 0 ? 0 : ca;
+					}
+					break;
+				}else {//部分是坑，有端点
+					int[] youduan = xiuz.get(leftDuan);
+					int[] zuoduan = xiuz.get(rightDuan);
+					int gao = arr[zuoduan[0]][zuoduan[1]];
+					int yougao = arr[youduan[0]][youduan[1]];
+					gao = gao > yougao ? yougao : gao;
+					for(int[] no : lianxuKen) {
+						int ca = arr[no[0]][no[1]] + shendu[no[0]][no[1]];
+						if(ca < gao) {
+							gao = ca;
+						}
+					}
+					//开始修正
+					for(int[] no : xiuz) {
+						int ca = gao - arr[no[0]][no[1]];
+						shendu[no[0]][no[1]] = ca < 0 ? 0 : ca;
+					}
+					//只移除连续坑
+					xiuz.removeAll(lianxuKen);
+				}
+				
+			}
 		}
-		return 0;
+		//深度表打印---直接计算总和
+		int zonghe = 0;
+		for(int[] s : shendu) {
+			System.out.println();
+			for(int x : s) {
+				System.out.print(x + ",");
+				zonghe += x;
+			}
+		}
+		return zonghe;
 	}
 }
