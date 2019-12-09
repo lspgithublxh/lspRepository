ed8e91193377f4e0a33125d4fdeae055c22757c6
lishaoping <1913796030@qq.com>
2019-12-09 14:22:56 +0800

    BR问题

--- KeyListBase/src/main/java/com/li/shao/ping/KeyListBase/datastructure/util/BRUtil.java
+++ KeyListBase/src/main/java/com/li/shao/ping/KeyListBase/datastructure/util/BRUtil.java
@@ -2,6 +2,7 @@ package com.li.shao.ping.KeyListBase.datastructure.util;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 四大静态性质：1.根黑 
@@ -25,6 +26,7 @@ import lombok.experimental.Accessors;
  * @date 2019年12月6日
  * @package  com.li.shao.ping.KeyListBase.datastructure.util
  */
+@Slf4j
 public class BRUtil {
 
 	
@@ -183,13 +185,126 @@ public class BRUtil {
 	
 	/**
 	 * 删除结点：
-	 *  如果有右分支：找出右分支最小值，删除，加到这个位置;;
+	 *  如果有右分支：找出右分支最小值，递归删除，加到这个位置;;
 	 *  如果只有左分支：找出左分支最大值，删除，加到这个位置;
 	 *  如果无分支：直接删除
-	 *  ----删除 判断： 是红色，则直接删除；是黑色：
+	 *  ----有分支的删除 判断： 是红色，则直接删除；是黑色：--则直接消除了这个分支：无影响。可以删除---条件是有兄弟结点：：否则要继续判断-往上找到红结点---如果没有--只能到根节点-左结点右旋本结点变红
+	 *  ----无分支的删除判断： 是红色，直接删除；是黑色：--如果有兄弟结点，直接删除；如果没有：则这个分支线需要向上，找到一个结点
+	 * --
+	 * 直接看黑色结点的删除方法：如果有单一分支，如果有多分支---子为红：可以变黑上进(非旋转)//无子黑，单子黑，双子黑
 	 * @param node
 	 */
 	private void deleteNode(Node node) {
+		//
+		
+	}
+	
+	/**
+	 * 平衡式删除黑色无子结点---层层往上：：形成了短1黑分支
+	 * @param node
+	 */
+	private void balanceDeleteBlackLeafNode(Node node) {
+		if(node.red || node.left != null || node.right != null) {
+			log.error("children not null error");
+			return;
+		}
+		Node p = node.parent;
+		if(p == null) {
+			log.info("delete ok ,root ");
+			return;
+		}
+		//先删除
+		Node b = null;
+		if(p.right == node) {
+			b = p.left;
+			p.right = null;
+			node.parent = null;
+		}else {
+			b = p.right;
+			p.left = null;
+			node.parent = null;
+		}
+		//无需再平衡
+		if(b != null) {
+			return;
+		}
+		if(p.red) {
+			p.red = false;
+			return;
+		}
+		//注意：组内均衡，和需要一直上升平衡到根结点
+		//短黑分支的顶
+		Node top = p.parent;
+		Node cur = p;
+		while(top != null) {//不是根结点
+			if(top.red) {//无它子直接变黑， 有则它子直接旋转  ---无需到根结点
+				if(top.right == cur) {
+					Node left = top.left;
+					if(left == null) {
+						top.red = false;
+						return;
+					}else {//直接右旋
+						rightRotate(left);
+						return;
+					}
+				}else {
+					Node right = top.right;
+					if(right == null) {
+						top.red = false;
+						return;
+					}else {//直接右旋
+						leftRotate(right);
+						return;
+					}
+				}
+			}else {//top是黑色 ----需要到根结点
+				if(top.right == cur) {
+					Node left = top.left;
+					if(left == null) {
+						cur = top;
+						top = cur.parent;//继续
+						continue;
+					}
+					if(left.red) {//红色有用--直接上旋, 再变黑--；；就。。黑色需要均衡---子变红：
+						left.red = false;
+						rightRotate(left);
+						return;
+					}else {//黑色，变红，右选，再继续
+						top.red = true;
+						rightRotate(left);
+						cur = left;//继续
+						top = left.parent;
+						continue;
+					}
+				}else {
+					Node right = top.right;
+					if(right == null) {
+						cur = top;
+						top = cur.parent;//继续
+						continue;
+					}
+					if(right.red) {//红色有用--直接上旋, 再变黑--；；就。。黑色需要均衡---子变红：
+						right.red = false;
+						leftRotate(right);
+						return;
+					}else {//黑色，变红，右选，再继续
+						top.red = true;
+						leftRotate(right);
+						cur = right;//继续
+						top = right.parent;
+						continue;
+					}
+				}
+			}
+		}
+		//已经到根结点，退出，完成
+	}
+	
+	/**
+	 * 平衡式删除红色无子结点
+	 * @param node
+	 */
+	private void balanceDeleteRedLeafNode(Node node) {
 		
 	}
 	
