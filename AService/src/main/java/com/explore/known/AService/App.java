74d50c9be55a6ea70553ea14fbbcf0c8ddb4ddfc
lishaoping <1913796030@qq.com>
2020-03-04 17:27:43 +0800

    spring-cloud消费者-生产者-注册中心基本

---
+++ AService/src/main/java/com/explore/known/AService/App.java
@@ -0,0 +1,13 @@
+package com.explore.known.AService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ RegistCenter/src/main/java/com/explore/known/RegistCenter/Main.java
@@ -0,0 +1,19 @@
+package com.explore.known.RegistCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Main.class, args);
+    }
+}
