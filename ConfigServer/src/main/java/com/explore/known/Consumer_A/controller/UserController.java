66d22190a18c9c338106e480d193fe23068db957
lishaoping <1913796030@qq.com>
2020-03-09 15:56:58 +0800

    配置服务器

---
+++ ConfigServer/src/main/java/com/explore/known/Consumer_A/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.explore.known.Consumer_A.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController("UserController")
+@RequestMapping("/api/consumer")
+public class UserController {
+
+	@Autowired
+	private RestTemplate template;
+    public static  final String URL_PREFIX = "http://localhost:8001";
+
+	
+	@GetMapping("/start/{user_id}")
+	public String hello(@PathVariable("user_id") Long id) {
+		return template.getForObject(URL_PREFIX + "/api/service/start/" + id, String.class);
+	}
+}
