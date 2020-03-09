7322706e75b37bba36a8fa9c383922639e0e0916
lishaoping <1913796030@qq.com>
2020-03-09 11:04:25 +0800

    客户端负载均衡，客户端通过服务名访问服务

---
+++ Service-B/src/main/java/com/explore/known/Service_A/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.explore.known.Service_A.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("usercontroller")
+@RequestMapping("/api/serviceB")
+public class UserController {
+
+	@GetMapping("/start/{user_id}")
+	public String hello(@PathVariable("user_id")Long id) {
+		return "this is a json data" + id;
+	}
+}
