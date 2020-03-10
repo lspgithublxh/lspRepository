f09b872e53332f4c9e0752dd2dc0a104373d02dc
lishaoping <1913796030@qq.com>
2020-03-10 11:27:23 +0800

    断路器监控hystrixboard成功

---
+++ Consumer-E-HystrixDashboard/src/main/java/com/explore/known/Consumer_A/Main.java
@@ -0,0 +1,23 @@
+package com.explore.known.Consumer_A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableZuulProxy
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ Consumer-E-HystrixDashboard/src/main/java/com/explore/known/Consumer_A/configuration/ConfigurationBean.java
@@ -0,0 +1,27 @@
+package com.explore.known.Consumer_A.configuration;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+
+@Configuration
+public class ConfigurationBean {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+	
+	@Bean
+	public ServletRegistrationBean hystrixMetricsStreamServlet() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+		registration.addUrlMappings("/hystrix.stream");
+		return registration;
+	}
+	
+}
