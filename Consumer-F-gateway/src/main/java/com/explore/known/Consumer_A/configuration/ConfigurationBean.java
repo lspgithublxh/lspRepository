91bdf3bceb592dd6ab9c0ffa8967282c164d7bb3
lishaoping <1913796030@qq.com>
2020-03-11 16:15:27 +0800

    gateway 普通过滤器-如增加header; 和全局过滤器和自定义过滤器：比如参数token的检查

--- Consumer-F-gateway/src/main/java/com/explore/known/Consumer_A/configuration/ConfigurationBean.java
+++ Consumer-F-gateway/src/main/java/com/explore/known/Consumer_A/configuration/ConfigurationBean.java
@@ -8,9 +8,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.explore.known.Consumer_A.filters.TokenFilter;
+
 /**
  * 限流，熔断，缓存
  *
+ * 断言： 路径/报头/cookie/请求方法/参数名与值
+ * pre:前过滤
+ * post:后过滤
+ * 
  * @author lishaoping
  * @date 2020年3月11日
  * @package  com.explore.known.Consumer_A.configuration
@@ -26,9 +32,10 @@ public class ConfigurationBean {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p -> p.path("/get")// 原路径
-						.filters(f -> f.addRequestHeader("Hello", "World"))
-										.uri("http://httpbin.org:80"))// 转发路径
+				//以下会覆盖yml里的配置--而不是merge
+//				.route(p -> p.path("/get")// 原路径
+//						.filters(f -> f.addRequestHeader("Hello", "World"))
+//										.uri("http://httpbin.org:80"))// 转发路径
 				.route(p -> p.host("*.hystrix.com")// host条件满足 才进入
 						.filters(f -> f.hystrix(config -> 
 												config.setName("mycmd")
@@ -38,5 +45,10 @@ public class ConfigurationBean {
 							.filters(f -> f.rewritePath("/hello(?<segment>.*)", "/api/consumer/hello${segment}"))
 							.uri("http://localhost:9021/"))
 				.build();
+	}
+	
+	@Bean
+	public TokenFilter tokenFilter(){
+	        return new TokenFilter();
 	}
 }
